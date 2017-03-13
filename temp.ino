c1218b2defef17340a5eade38f4d0c8c699677d7
SMLunchen <github@schwarzes-seelenreich.de>
2017-03-13 01:55:23 +0100

    Update temp.ino

--- temp.ino
+++ temp.ino
@@ -9,11 +9,16 @@ DallasTemperature sensors(&oneWire);
 int hbled = 13;
 int trans = 7;
 int dts = 0;
+int debugpi = 9;
+int debugpi2 = 8;
+int msstate = 1;
+float vref = 5.0;
+int vBat_raw = 0;
+float vBat = 0;
 
 
 
-#define PACKET_SIZE 11
-
+//so solls mal aussehen... :X
 typedef struct sensorData_t{
   byte stat;
   byte sensorId;
@@ -32,6 +37,9 @@ void setup()
 	 sensors.begin();
 	 pinMode(hbled, OUTPUT);
 	 pinMode(trans, OUTPUT);
+	 pinMode(debugpi, OUTPUT);
+	 pinMode(debugpi2, OUTPUT);
+
 }
 
 //void prepareData(int t_pre) {
@@ -77,6 +85,13 @@ void loop()
 	//leave LED high during measurement and transmission
 	digitalWrite(hbled, 1);
 
+	//get LiPo voltage to estimate charging status
+	Serial.println();
+	int vBat_raw = analogRead(A0);
+	vBat = vBat_raw*vref/1024;
+	Serial.print("Bat Voltage: ");
+	Serial.println(vBat);
+
 
 	Serial.print(" Requesting temperatures...");
 	sensors.requestTemperatures();
@@ -84,71 +99,114 @@ void loop()
 	Serial.print("Temperature is: ");
 	Serial.println(sensors.getTempCByIndex(0));
 
-	//converting Stuff
+	//get temp
 	float t_mess=(sensors.getTempCByIndex(0));
+	//convert float to int
 	int t_raw=t_mess*100;
+	//debugvalue:
+	//int t_raw=25600;
+
+	//debug
 	Serial.print("T-RAWR: ");
 	Serial.println(t_raw,BIN);
-	//prepareData(t_raw);
-	//build array
+
+	//build 5 digit array
 	char buffer[5];
-		dtostrf(t_raw, 5, 0, buffer);
+
+	dtostrf(t_raw, 5, 0, buffer);
 		for (int i = 0; i < 5 ; i++)
 		{
-			//clocking msstate
-			int msstate = 0;
+				//sub T prepare to send
 				int t_pts = buffer[i];
-				Serial.print(t_pts,BIN);
-				Serial.print(" ");
+				//serial debug the whole buffer as bin
+				Serial.print("BufferVAL: ");
+				Serial.print(char(t_pts));
+				Serial.print(" - ");
+				Serial.println(char(t_pts),BIN);
 				delay(50);
-				for (int ii = 0; ii < 8; ii++)
-				{
-				int t_rts = bitRead(t_pts,ii);
-				Serial.print((bitRead(t_rts, ii)));
-				Serial.print(" : ");
 
-				switch (msstate)
+				//clocking data out of Pin 7
+				for (int ii = 0; ii < 6; ii++)
+				{
+				//take it bitvise! -- ready to send
+				int t_rts = bitRead(t_pts,ii);
+
+				//only debug
+				Serial.print(t_rts);
+				Serial.print(" : ");
+				if (ii == 5){
+					Serial.println();
+				}
+
+
+
+				//main function
+				for (int iii = 0; iii < 2; iii++)
+				{
+
+				//debugging clock part 1
+				digitalWrite(debugpi, msstate);
+				//delay(1);
+				//debugging data part 1
+				digitalWrite(debugpi2, t_rts);
+
+				switch (t_rts)
 				{
 				case 0:
 				{
-						if  ( (msstate == 0) &&  (t_rts == 1) ) {
-
+						if   (msstate == 1) {
+						//clocking dataline -> rising edge
 						dts = 1;
 						digitalWrite(trans, dts);
-						delay(2);
-						msstate = 1;
+						delay(1);
 					}
-						if ( (msstate == 0) &&  (t_rts == 0) ) {
+						else if (msstate == 0) {
+						//clocking dataline -> falling edge
 						dts = 0;
 						digitalWrite(trans, dts);
-						delay(2);
-						msstate = 1;
+						delay(1);
 						}
 					}
 				break;
 
 				case 1:
 				{
-					if  ( (msstate == 1) &&  (t_rts == 1) ) {
-
-						dts = 0;
-						digitalWrite(trans, dts);
-						delay(2);
-						msstate = 0;
-					}
-					if ( (msstate == 1) &&  (t_rts == 0) ) {
+					if  ( msstate == 0 ) {
+						//clocking dataline -> rising edge
 						dts = 1;
 						digitalWrite(trans, dts);
-						delay(2);
-						msstate = 0;
-					}
+						delay(1);
+						}
+					else if ( msstate == 1 ) {
+						//clocking dataline -> rising edge
+						dts = 0;
+						digitalWrite(trans, dts);
+						delay(1);
+						}
 				}
 				break;
-
+				//end Switch
 				}
-		}
-		}
 
+				//clocking msstate
+				if (msstate == 1 ) {
+					msstate = 0;
+				}
+				else{
+					msstate = 1;
+				}
+
+
+
+				//end for
+				}
+
+
+
+
+		}
+		}
+	Serial.println();
 	delay (200);
 	digitalWrite(hbled, 0);
 	delay(1000);
